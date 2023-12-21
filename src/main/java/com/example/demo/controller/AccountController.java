ddabbf32beeb7296ba615dc1411a6106ac4e7019
ha <some@email.com>
2023-12-21 19:31:52 +0700

    second commit

---
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -0,0 +1,131 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Account;
+import com.example.demo.model.AccountService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.Objects;
+
+
+@Controller
+public class AccountController {
+    @Autowired
+    private AccountService sv;
+    @GetMapping("")
+    public String index(HttpSession session, Model model){
+        Account account = (Account) session.getAttribute("loggedAccount");
+        if(account != null) {
+            model.addAttribute("account", account);
+            return "index";
+        }
+
+        return "login";
+
+    }
+
+    @GetMapping("/login")
+    public String login(HttpSession session, HttpServletResponse res) throws IOException {
+        if((Account)session.getAttribute("loggedAccount")!=null){
+            res.sendRedirect("");
+        }
+        return "login";
+    }
+    @PostMapping("/login")
+    public String login(@ModelAttribute Account account, HttpSession session,Model model) throws Exception {
+
+        // Call service xử lý đăng nhập
+        Account loggedAccount = sv.findByUsername(account.getUsername());
+
+        if(!Objects.equals(loggedAccount.getPassword(), account.getPassword())) {
+            model.addAttribute("error", "Mật khẩu không chính xác");
+            return "login";
+
+        }
+
+        else{
+
+
+        // Lưu user vào session
+        session.setAttribute("loggedAccount", loggedAccount);
+
+        return "index";}
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error";
+    }
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("account", new Account());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistration(@ModelAttribute Account account,Model model) {
+
+
+        //validate username đã tồn tại chưa
+        if(sv.findByUsername(account.getUsername()) != null) {
+            model.addAttribute("error", "Mật khẩu không chính xác");
+            return "register";
+        }
+        sv.addAccount(account);
+        model.addAttribute("success", true);
+
+        //kiểm tra các lỗi
+        return "index";
+
+    }
+
+    @GetMapping("/change_password")
+    public String changePass(){
+        return "changePassword";
+    }
+    @PostMapping("/change_password")
+    public String changePw(
+            @RequestParam String oldPassword,
+            @RequestParam String newPassword,
+            HttpSession session) throws Exception {
+
+        Account account = (Account) session.getAttribute("loggedAccount");
+        System.out.println(account.toString());
+
+        sv.changePassword(account, oldPassword, newPassword);
+
+        return "index";
+    }
+
+    @GetMapping("/product")
+    public String product(){
+        return "product_manage";
+    }
+
+    @GetMapping("/profile")
+    public String profile(HttpSession session,Model model){
+        Account account = (Account) session.getAttribute("loggedAccount");
+        model.addAttribute("account",account);
+
+        return "profile_manage";
+    }
+
+    @GetMapping("/saleManagement")
+    public String saleManage(){
+        return "sale_manage";
+    }
+
+    @GetMapping("/staffManagement")
+    public String staffManage(){
+        return "staff_manage";
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/AccountService.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class AccountService {
+
+    @Autowired
+    private AccountRepository repo;
+
+
+
+    public Account addAccount(@RequestBody Account account) {
+        if (account.getRole() == null) {
+            account.setRole("user");
+        }
+
+        //account.setPassword(encoder.encode(account.getPassword())); // hash
+        repo.save(account);
+        return account;
+    }
+    public Account findById(int id)
+    {
+        return repo.findById(id).orElse(null);
+    }
+    public Account findByUsername(String username)
+    {
+        return repo.findByUsername(username);
+    }
+
+    public List<Account> getAllAccount() {
+        return repo.findAll();
+    }
+    public void changePassword(Account account,String oldPassword, String newPassword) throws Exception {
+        Account dbAccount = repo.findById(account.getId()).get();
+        if(!Objects.equals(dbAccount.getPassword(), oldPassword)) {
+            throw new Exception("Mật khẩu cũ không chính xác");
+        }
+
+        // Mã hóa mật khẩu mới
+
+
+        // Cập nhật mật khẩu mới lên DB
+        dbAccount.setPassword(newPassword);
+        repo.save(dbAccount);
+
+    }
+
+    }
+
+
